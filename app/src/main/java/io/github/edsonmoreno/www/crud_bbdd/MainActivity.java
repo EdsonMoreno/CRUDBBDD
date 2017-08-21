f07117938eadfaa186c1e8a422a7d80cf85f82f5
Edson D. Moreno S <diego2816@gmail.com>
2017-08-21 00:47:04 -0400

    se desarrollo el codigo necesario para generar una consulta a la base de datos y mostrar en pantalla el nombre y apellido del id dado

--- app/src/main/java/io/github/edsonmoreno/www/crud_bbdd/MainActivity.java
+++ app/src/main/java/io/github/edsonmoreno/www/crud_bbdd/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.edsonmoreno.www.crud_bbdd;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 // Insert the new row, returning the primary key value of the new row
                 long newRowId = db.insert(EstructuraBaseDeDatos.NOMBRE_TABLA, null, values);
 
-                Toast.makeText(getApplicationContext(),"Se guardo el registr con clave: "
+                Toast.makeText(getApplicationContext(),"Se guardo el registroo con clave: "
                         +newRowId, Toast.LENGTH_LONG).show();
             }
         });
@@ -67,7 +68,49 @@ public class MainActivity extends AppCompatActivity {
         buscar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                /**
+                 * Para leer de una base de datos, utiliza el método query()
+                 * , y pasa tus criterios de selección y columnas deseadas.
+                 * El método combina elementos de insert() y update(), excepto
+                 * que la lista de columnas define los datos que deseas obtener
+                 * , en lugar de los datos a insertar. Los resultados de la
+                 * consulta se devuelven en un objeto Cursor.
+                 */
+                //hacer que se pueda leer la base de datos
+                  SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
+                    /* Columnas de la base de datos consultadas
+                    g* segun el criterio de ebusqueda
+                    en este caso siendo criterio el id para que nos
+                    devuelva nomvre y apellido
+                    * .*/
+                  String[] projection = {EstructuraBaseDeDatos.NOMBRE_COLUMNA_DOS,
+                                          EstructuraBaseDeDatos.NOMBRE_COLUMNA_TRES
+                                          };
+
+                // Filter results WHERE "title" = 'My Title'
+                String selection = EstructuraBaseDeDatos.NOMBRE_COLUMNA_UNO + " = ?";
+                //ubicamos elcriterio de busqueda elegido por nosotros
+                String[] selectionArgs = { texto_id.getText().toString() };
+
+                // Si hayresultados iguales los ordena segun un criterio
+               /* String sortOrder =
+                        FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";*/
+
+                Cursor c = db.query(
+                        EstructuraBaseDeDatos.NOMBRE_TABLA,       // The table to query
+                        projection,                               // The columns to return
+                        selection,                                // The columns for the WHERE clause
+                        selectionArgs,                            // The values for the WHERE clause
+                        null,                                     // don't group the rows
+                        null,                                     // don't filter by row groups
+                        null                                 // The sort order
+                );
+                //Ubicamos elcursor en el primer registro de la tabla en memoria
+                c.moveToFirst();
+                //obtenemos la informacion de la primera columna de la tabla resultado
+                texto_nombre.setText(c.getString(0));
+                texto_apellido.setText(c.getString(1));
             }
         });
 
