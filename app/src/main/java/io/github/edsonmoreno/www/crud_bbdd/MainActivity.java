30726634316480b0aaa0054bd82de92de7bab40c
Edson D. Moreno S <diego2816@gmail.com>
2017-08-20 20:29:49 -0400

    Completado conjunto de instrucciones que permites introducir datos en la BBDD, utilizando el boton insertar de la interfaz grafica

--- app/src/main/java/io/github/edsonmoreno/www/crud_bbdd/MainActivity.java
+++ app/src/main/java/io/github/edsonmoreno/www/crud_bbdd/MainActivity.java
@@ -1,10 +1,15 @@
 package io.github.edsonmoreno.www.crud_bbdd;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +29,31 @@ public class MainActivity extends AppCompatActivity {
         texto_apellido = (EditText) findViewById(R.id.aapellido);
         texto_nombre = (EditText) findViewById(R.id.nombre);
 
+        //instanciadapara acceder a labase de datos
+        final BBDDHelper mDbHelper = new BBDDHelper(this);
+
         //Crendo eventos
         insertar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                // Gets the data repository in write mode
+                SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
+                // Create a new map of values, where column names are the keys
+                ContentValues values = new ContentValues();
+                //obtenemos el valor del campo uno y lo guardamos en la base de datos
+                //una para cada tabla
+                values.put(EstructuraBaseDeDatos.NOMBRE_COLUMNA_UNO, texto_id.getText().toString());
+                values.put(EstructuraBaseDeDatos.NOMBRE_COLUMNA_DOS, texto_nombre.getText().toString());
+                values.put(EstructuraBaseDeDatos.NOMBRE_COLUMNA_TRES, texto_apellido.getText().toString());
+
+                //values.put(EstructuraBaseDeDatos.COLUMN_NAME_SUBTITLE, subtitle);
+
+                // Insert the new row, returning the primary key value of the new row
+                long newRowId = db.insert(EstructuraBaseDeDatos.NOMBRE_TABLA, null, values);
+
+                Toast.makeText(getApplicationContext(),"Se guardo el registr con clave: "
+                        +newRowId, Toast.LENGTH_LONG).show();
             }
         });
 
